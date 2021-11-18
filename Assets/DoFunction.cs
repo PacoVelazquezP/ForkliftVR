f3a99995c9dd5663bdfad79e3688cea7487de6b5
PacoVelazquezP <87715200+PacoVelazquezP@users.noreply.github.com>
2021-11-18 12:57:32 -0600

    Test

---
+++ Assets/DoFunction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoFunction : MonoBehaviour
+{
+    public GameObject txtStart;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Barril")){
+            txtStart.SetActive(true);
+        }
+    }
+}
